0bb0da98ef99e48e5375b8ac74b0f3803c4d7b3a
Brandy Mello <brandyamello@gmail.com>
2019-05-31 21:08:33 -0600

    Set up todo list js file Architecture and Data Model

---
+++ todo-list.js
@@ -0,0 +1,24 @@
+class ToDoList {
+  constructor() {
+    this.id = id;
+    this.title = title; //string
+    this.urgent = urgent; //boolean
+    this.tasks = tasks; //array
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFormStorage() {
+
+  }
+
+  updateToDo() {
+
+  }
+
+  updateTask() {
+    
+  }
+}
