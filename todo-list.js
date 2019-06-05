c7550ece82961ae8439d761418c6c36881b7faf9
Brandy Mello <brandyamello@gmail.com>
2019-06-04 21:39:34 -0600

    Clean files

--- todo-list.js
+++ todo-list.js
@@ -1,12 +1,11 @@
 class ToDoList {
   constructor(list) {
     this.id = list.id;
-    this.title = list.title; //string
-    this.urgent = list.urgent; //boolean
-    this.tasks = list.tasks; //array of objects
+    this.title = list.title;
+    this.urgent = list.urgent;
+    this.tasks = list.tasks;
   }
 
- 
   saveToStorage(cardsArray) {
     localStorage.setItem('todos', JSON.stringify(cardsArray));
   }
@@ -33,23 +32,4 @@ class ToDoList {
   }
 }
 
-function updateStar(e, id) {
-  var starToUpdate = e.target;
-  var activeStar = 'images/star-active.svg';
-  var inactiveStar = 'images/star.svg';
-  if (ideaList[id].star === true) {
-    starToUpdate.src = activeStar;
-  } else {
-    starToUpdate.src = inactiveStar;
-  }
-}
-
-function triggerStar(e) {
-    if (e.target.id === 'card__img--star') {
-      var index = findKey(e);
-      ideaList[index].star = !ideaList[index].star;
-      ideaList[index].updateIdea(ideaList[index].title, ideaList[index].body, ideaList[index].star);
-      updateStar(e, index);
-    }
-  }
 
